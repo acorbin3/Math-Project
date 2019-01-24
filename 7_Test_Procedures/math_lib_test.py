e70edb751fb1e2dcb9a59c46e1ce497680d1b46a
Adam Corbin <acorbin3@gmail.com>
2019-01-24 12:52:03 -0500

    #1 Updating per informal review
    Updating per comments in informal review here 97695a09e11fbc80dd82331befc91fefb8750269
    Creating branch due to severity and needs to be reviewed by someone else

--- 7_Test_Procedures/math_lib_test.py
+++ 7_Test_Procedures/math_lib_test.py
@@ -2,13 +2,13 @@ from math_lib import pow_native
 def test_res(res):
     return "Pass" if res else "Fail"
 
-print("Match_TC_1 result: " + test_res(pow_native(0, 0) == 1))
-print("Match_TC_2 result: " + test_res(pow_native(10, 0) == 1))
+print("Math_TC_1 result: " + test_res(pow_native(0, 0) == 1))
+print("Math_TC_2 result: " + test_res(pow_native(10, 0) == 1))
 
-print("Match_TC_3 result: " + test_res(pow_native(2, 2) == 4))
-print("Match_TC_4 result: " + test_res(pow_native(3, 5) == 243))
+print("Math_TC_3 result: " + test_res(pow_native(2, 2) == 4))
+print("Math_TC_4 result: " + test_res(pow_native(3, 5) == 243))
 
-print("Match_TC_5 result: " + test_res(pow_native(4, -4) == 0.00390625))
-print("Match_TC_5 result: " + test_res(pow_native(2, -7) == 1))
+print("Math_TC_5 result: " + test_res(pow_native(4, -4) == 0.00390625))
+print("Math_TC_6 result: " + test_res(pow_native(2, -7) == 0.00781251))
 
-print("Match_TC_7 result: " + test_res(pow_native(0, -1) == 1))
+print("Math_TC_7 result: " + test_res(pow_native(0, -1) == 1))
