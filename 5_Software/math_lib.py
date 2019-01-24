f6595f8f60bb99f860fe6a821a551158a5100e28
Adam Corbin <acorbin3@gmail.com>
2019-01-24 09:38:22 -0500

    #1 adding content

--- 5_Software/math_lib.py
+++ 5_Software/math_lib.py
@@ -1,1 +1,13 @@
-
+def pow_native(number, exponent):
+    """
+    TODO - add function overview
+    :param number:
+    :param exponent:
+    :return:
+    """
+    if exponent == 0:
+        return 1
+    total = 0
+    for i in range(0,exponent+1):
+        total += number
+    return total
